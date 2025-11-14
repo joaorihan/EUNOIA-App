34247a68d25888f6a86e5a9c00ec0e0237e4a48e
joaorihan <joao.rihan@ppro.com>
2025-11-14 15:42:18 -0300

    feat(config): setup environment variables for API keys

---
+++ app.config.js
@@ -0,0 +1,39 @@
+export default {
+  expo: {
+    name: "EUNOIA",
+    slug: "eunoia",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "eunoia",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.joaorihan",
+      googleServicesFile: "./GoogleService-Info.plist"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      googleServicesFile: "./google-services.json",
+      package: "com.joaorihan"
+    },
+    plugins: [
+      "@react-native-firebase/app",
+      "@react-native-firebase/auth"
+    ],
+    extra: {
+      geminiApiKey: process.env.GEMINI_API_KEY || ""
+    }
+  }
+};
+
